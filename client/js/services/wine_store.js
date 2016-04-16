539b72d2ada6ee19634e9b9acb1a86e2376f1d45
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-04-16 20:38:43 +0200

    wip: store wines and display them in linst

--- client/js/services/wine_store.js
+++ client/js/services/wine_store.js
@@ -1,13 +1,17 @@
-export var wineStore = function($q){
 
-	var wines = [{
-		name: 'lala'
-	}];
+
+//http://services.wine.com/api/beta2/service.svc/json/catalog?apikey=06bfbef7d9ef00636951cb303e397bf3
+export var wineStore = function($http, $q){
 
 	function all(){
 		return $q((resolve) => {
-			setTimeout(() => resolve(wines));
+			setTimeout(() => resolve(JSON.parse(localStorage.getItem('wines'))));
 		});
+		/*return $http.get('http://services.wine.com/api/beta2/service.svc/json/catalog?apikey=06bfbef7d9ef00636951cb303e397bf3')
+			.then((result) => {
+				localStorage.setItem('wines', JSON.stringify(result.data.Products.List));
+				return result.data.Products.List;
+			})*/
 	}
 
 	return {
