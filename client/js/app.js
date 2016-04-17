acb8af1403d35bd8fcd62642c9133f330f9c7244
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-04-17 22:25:20 +0200

    add wineAPI service to module

--- client/js/app.js
+++ client/js/app.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
 import {wineStore} from './services/wine_store';
+import {wineAPI} from './services/wine_api';
 
 import {routes} from './routes';
 
@@ -9,3 +10,4 @@ var app = angular.module('wineApp', ['ui.router']);
 app.config(routes);
 
 app.service('wineStore', wineStore);
+app.service('wineAPI', wineAPI);
