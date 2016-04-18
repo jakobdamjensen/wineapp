a3b1e027155eff89da84251c6c90c9ce0f4ab78e
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-04-18 20:29:07 +0200

    adds ng animate

--- client/js/app.js
+++ client/js/app.js
@@ -1,13 +1,16 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import ngAnimate from 'angular-animate';
+
 
 import {wineStore} from './services/wine_store';
 import {wineAPI} from './services/wine_api';
 
 import {routes} from './routes';
 
-var app = angular.module('wineApp', ['ui.router']);
+var app = angular.module('wineApp', ['ui.router', 'ngAnimate']);
 app.config(routes);
 
 app.service('wineStore', wineStore);
 app.service('wineAPI', wineAPI);
+
