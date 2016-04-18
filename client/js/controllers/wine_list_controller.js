6c98352e319bdc326fb3292d9ea333cdbcba4659
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-04-18 20:29:42 +0200

    list controller now watches wines list in wineStore service

--- client/js/controllers/wine_list_controller.js
+++ client/js/controllers/wine_list_controller.js
@@ -1,7 +1,13 @@
-export var wineListController = function(wineStore){
+export var wineListController = function(wineStore, $scope){
 	var model = this;
 
 	model.wines = [];
 
+	$scope.$watch(() => wineStore.wines, (newval) => {
+		model.wines = newval;
+	});
+
 	wineStore.all().then((wines) => model.wines = wines);
+
+
 };
